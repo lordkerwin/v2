5abc5c8f2eab9b23b08b278850a31cf63c81fd53
Sean Kerwin <swkerwin@icloud.com>
2022-05-24 16:41:00 +0100

    fix for menu button

--- components/Menu.js
+++ components/Menu.js
@@ -36,32 +36,10 @@ const Menu = () => {
     return (
         <div className="mobile-menu">
             <div ref={wrapperRef} className="flex">
+                {/* Menu Button */}
                 <button onClick={() => toggleMenu()} className="menu-btn">
-                    <svg
-                        width="300"
-                        height="300"
-                        viewBox="0 0 32 42"
-                        xmlns="http://www.w3.org/2000/svg"
-                        className={cn({ active: open })}
-                    >
-                        <g transform="matrix(1,0,0,1,-389.5,-264.004)">
-                            <g id="arrow_left2">
-                                <g transform="matrix(1,0,0,1,0,5)">
-                                    <path
-                                        id="top"
-                                        d="M390,270L420,270L420,270C420,270 420.195,250.19 405,265C389.805,279.81 390,279.967 390,279.967"
-                                    />
-                                </g>
-                                <g transform="matrix(1,1.22465e-16,1.22465e-16,-1,0.00024296,564.935)">
-                                    <path
-                                        id="bottom"
-                                        d="M390,270L420,270L420,270C420,270 420.195,250.19 405,265C389.805,279.81 390,279.967 390,279.967"
-                                    />
-                                </g>
-                                <path id="middle" d="M390,284.967L420,284.967" />
-                            </g>
-                        </g>
-                    </svg>
+                    <MenuAlt3Icon className={cn('w-8 h-8', { hide: open })} />
+                    <XIcon className={cn('w-8 h-8', { hide: !open })} />
                 </button>
                 <aside className={cn('sidebar', { open: open })}>
                     <nav ref={navRef}>
