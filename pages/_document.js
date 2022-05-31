bba82b49e8a4d2c32713aa66dc2bd24fd6f03549
Sean Kerwin <swkerwin@icloud.com>
2022-05-31 23:22:08 +0100

    work on services

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,7 @@ export default function Document() {
         <Html>
             <Head>
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
                 <link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono&display=swap" rel="stylesheet" />
                 <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
             </Head>
