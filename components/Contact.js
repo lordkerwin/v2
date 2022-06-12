c22c31058a1acf832905ef68a347652f39d78fef
Sean Kerwin <swkerwin@icloud.com>
2022-06-12 14:57:42 +0100

    wip

--- components/Contact.js
+++ components/Contact.js
@@ -1,48 +1,143 @@
+import Input from '@/components/Input'
+import Textarea from '@/components/Textarea'
+import { RefreshIcon } from '@heroicons/react/solid'
 import React, { useEffect, useState } from 'react'
 
-const Contact = () => {
-    const [fullName, setFullName] = useState('bob')
-    const [email, setEmail] = useState('foo@bar.com')
-    const [message, setMessage] = useState('hello there...')
+const defaultErrors = {
+    fullName: '',
+    email: '',
+    message: '',
+}
 
-    const submit = async () => {
-        // e.preventDefault()
-        const data = {
-            fullName,
-            email,
-            message,
+const Contact = () => {
+    const [fullName, setFullName] = useState({ value: '', dirty: false, error: defaultErrors.fullName })
+    const [email, setEmail] = useState({ value: '', dirty: false, error: defaultErrors.email })
+    const [message, setMessage] = useState({ value: '', dirty: false, error: defaultErrors.message })
+    const [loading, setLoading] = useState(false)
+    const [sent, setSent] = useState(false)
+    const [formValid, setFormValid] = useState(false)
+
+    useEffect(() => {
+        if (!fullName.value && fullName.dirty) {
+            setFullName({ ...fullName, dirty: false, error: 'Please enter your name' })
+            setFormValid(false)
+        }
+        if (!email.value && email.dirty) {
+            setEmail({ ...email, dirty: false, error: 'Please enter your email address' })
+            setFormValid(false)
+        }
+        if (!message.value && message.dirty) {
+            setMessage({ ...message, dirty: false, error: 'Please enter your message' })
+            setFormValid(false)
         }
 
-        const res = await fetch('/api/sendgrid', {
+        if (fullName.value && email.value && message.value) {
+            setFormValid(true)
+        }
+    }, [fullName, email, message])
+
+    const resetForm = () => {
+        setFullName({ value: '', dirty: false, error: defaultErrors.fullName })
+        setEmail({ value: '', dirty: false, error: defaultErrors.email })
+        setMessage({ value: '', dirty: false, error: defaultErrors.message })
+        setFormValid(false)
+    }
+
+    const submit = async (e) => {
+        e.preventDefault()
+        if (sent) {
+            return
+        }
+        setLoading(true)
+
+        // send email
+        await fetch('/api/sendgrid', {
             body: JSON.stringify({
-                email: email,
-                fullname: fullName,
-                message: message,
+                email: email.value,
+                fullname: fullName.value,
+                message: message.value,
                 subject: 'Contact Form Submission',
             }),
             headers: {
                 'Content-Type': 'application/json',
             },
             method: 'POST',
+        }).then((res) => {
+            if (res.status === 200) {
+                setLoading(false)
+                setSent(true)
+                setTimeout(() => {
+                    resetForm()
+                    setSent(false)
+                }, 5000)
+            } else {
+                setLoading(false)
+            }
         })
-
-        const { error } = await res.json()
-        if (error) {
-            console.log(error)
-            return
-        }
-        console.log(fullName, email, message)
     }
+
+    const renderText = () => (sent ? 'Message Sent' : 'Send Message')
 
     return (
         <div id="contact" className="fp-section">
             <h2 className="section-title">Contact</h2>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, perferendis quo beatae inventore quibusdam
-                architecto ducimus maiores eligendi sunt cumque, vel aliquam explicabo alias obcaecati molestiae. Id
-                alias quae tempora!
-            </p>
-            <button onClick={() => submit()}>Click</button>
+            <div className="grid sm:grid-cols-3 gap-12 w-full">
+                <div className="w-full sm:col-span-2">
+                    <form className="" onSubmit={(e) => submit(e)}>
+                        <Input
+                            label="name"
+                            value={fullName.value}
+                            onChange={(val) => setFullName({ value: val, dirty: true })}
+                            error={fullName.error}
+                        />
+                        <Input
+                            label="email"
+                            value={email.value}
+                            onChange={(val) => setEmail({ value: val, dirty: true })}
+                            error={email.error}
+                        />
+                        <Textarea
+                            label="message"
+                            value={message.value}
+                            onChange={(val) => setMessage({ value: val, dirty: true })}
+                            error={message.error}
+                        />
+                        <button type="submit" className={`submit ${sent ? 'sent' : ''}`} disabled={!formValid}>
+                            {loading ? (
+                                <svg
+                                    className="animate-spin -ml-1 mr-3 h-6 w-6 text-white"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                >
+                                    <circle
+                                        className="opacity-25"
+                                        cx="12"
+                                        cy="12"
+                                        r="10"
+                                        stroke="currentColor"
+                                        strokeWidth="4"
+                                    ></circle>
+                                    <path
+                                        className="opacity-75"
+                                        fill="currentColor"
+                                        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                                    ></path>
+                                </svg>
+                            ) : (
+                                renderText()
+                            )}
+                        </button>
+                    </form>
+                </div>
+                <div>
+                    <h3>Let&apos;s work together</h3>
+                    <p>
+                        I&apos;m always looking for new opportunities to help. If you have a project in mind, or you
+                        just want to chat, I&apos;m always open to discussing it.
+                    </p>
+                </div>
+            </div>
         </div>
     )
 }
--- pages/api/sendgrid.js
+++ pages/api/sendgrid.js
@@ -4,15 +4,17 @@ sendgrid.setApiKey(process.env.SENDGRID_API_KEY)
 
 async function sendEmail(req, res) {
     try {
-        console.log('REQ.BODY', req.body)
         await sendgrid.send({
-            to: 'swkerwin@icloud.com', // Your email where you'll receive emails
-            from: 'howdy@seankerwin.dev', // your website email address here
+            to: 'hey@seankerwin.dev', // Your email where you'll receive emails
+            from: 'contact@seankerwin.dev', // your website email address here
             subject: `${req.body.subject}`,
-            html: `<div>Contact form submission</div>`,
+            html: `
+                <strong>Name:</strong><br>${req.body.fullname}<br /><br />
+                <strong>Email:</strong><br>${req.body.email}<br /><br />
+                <strong>Message:</strong><br>${req.body.message}
+            `,
         })
     } catch (error) {
-        // console.log(error);
         return res.status(error.statusCode || 500).json({ error: error.message })
     }
 
